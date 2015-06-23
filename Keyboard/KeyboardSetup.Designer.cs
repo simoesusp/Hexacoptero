33adefac06b56ab4d3046657e1ae2a9771d2174f
denisdifazio <denisdifazio@gmail.com>
2015-06-23 20:27:37 -0300

    Adicionando Keyboard
    Mudanças feitas no MissionPlanner para suportar o layout do teclado

---
+++ Keyboard/KeyboardSetup.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace MissionPlanner.Keyboard
+{
+    partial class KeyboardSetup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBarRoll = new MissionPlanner.Controls.HorizontalProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBarPitch = new MissionPlanner.Controls.HorizontalProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BUT_enable = new MissionPlanner.Controls.MyButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBarThrottle = new MissionPlanner.Controls.HorizontalProgressBar();
+            this.progressBarYaw = new MissionPlanner.Controls.HorizontalProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBarRoll
+            // 
+            this.progressBarRoll.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.progressBarRoll.Label = null;
+            this.progressBarRoll.Location = new System.Drawing.Point(55, 12);
+            this.progressBarRoll.Maximum = 2000;
+            this.progressBarRoll.maxline = 0;
+            this.progressBarRoll.Minimum = 1000;
+            this.progressBarRoll.minline = 0;
+            this.progressBarRoll.Name = "progressBarRoll";
+            this.progressBarRoll.Size = new System.Drawing.Size(100, 23);
+            this.progressBarRoll.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label1.Location = new System.Drawing.Point(10, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Roll";
+            // 
+            // progressBarPitch
+            // 
+            this.progressBarPitch.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.progressBarPitch.Label = null;
+            this.progressBarPitch.Location = new System.Drawing.Point(55, 41);
+            this.progressBarPitch.Maximum = 2000;
+            this.progressBarPitch.maxline = 0;
+            this.progressBarPitch.Minimum = 1000;
+            this.progressBarPitch.minline = 0;
+            this.progressBarPitch.Name = "progressBarPitch";
+            this.progressBarPitch.Size = new System.Drawing.Size(100, 23);
+            this.progressBarPitch.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label2.Location = new System.Drawing.Point(10, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Pitch";
+            // 
+            // BUT_enable
+            // 
+            this.BUT_enable.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.BUT_enable.Location = new System.Drawing.Point(181, 12);
+            this.BUT_enable.Name = "BUT_enable";
+            this.BUT_enable.Size = new System.Drawing.Size(75, 23);
+            this.BUT_enable.TabIndex = 23;
+            this.BUT_enable.Text = "Enable";
+            this.BUT_enable.UseVisualStyleBackColor = true;
+            this.BUT_enable.Click += new System.EventHandler(this.BUT_enable_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label3.Location = new System.Drawing.Point(8, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Throttle";
+            // 
+            // progressBarThrottle
+            // 
+            this.progressBarThrottle.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.progressBarThrottle.Label = null;
+            this.progressBarThrottle.Location = new System.Drawing.Point(55, 70);
+            this.progressBarThrottle.Maximum = 2000;
+            this.progressBarThrottle.maxline = 0;
+            this.progressBarThrottle.minline = 0;
+            this.progressBarThrottle.Name = "progressBarThrottle";
+            this.progressBarThrottle.Size = new System.Drawing.Size(100, 23);
+            this.progressBarThrottle.TabIndex = 25;
+            // 
+            // progressBarYaw
+            // 
+            this.progressBarYaw.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.progressBarYaw.Label = null;
+            this.progressBarYaw.Location = new System.Drawing.Point(55, 99);
+            this.progressBarYaw.Maximum = 2000;
+            this.progressBarYaw.maxline = 0;
+            this.progressBarYaw.minline = 0;
+            this.progressBarYaw.Name = "progressBarYaw";
+            this.progressBarYaw.Size = new System.Drawing.Size(100, 23);
+            this.progressBarYaw.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label4.Location = new System.Drawing.Point(10, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Yaw";
+            // 
+            // KeyboardSetup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBarYaw);
+            this.Controls.Add(this.progressBarThrottle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BUT_enable);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBarPitch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBarRoll);
+            this.Name = "KeyboardSetup";
+            this.Text = "Keyboard";
+            this.Load += new System.EventHandler(this.Keyboard_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Controls.HorizontalProgressBar progressBarRoll;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Controls.MyButton BUT_enable;
+        private Controls.HorizontalProgressBar progressBarPitch;
+        private System.Windows.Forms.Label label3;
+        private Controls.HorizontalProgressBar progressBarThrottle;
+        private Controls.HorizontalProgressBar progressBarYaw;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Keyboard/KeyboardSetup.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using MissionPlanner.Controls;
+using MissionPlanner.Utilities;
+using MissionPlanner.Keyboard;
+
+namespace MissionPlanner.Keyboard
+{
+    public partial class KeyboardSetup : Form
+    {
+        globalKeyboardHook gkh = new globalKeyboardHook();        
+
+        public KeyboardSetup()
+        {
+            InitializeComponent();
+            MissionPlanner.Utilities.Tracking.AddPage(this.GetType().ToString(), this.Text);
+        }
+
+        public void Keyboard_Load(object sender, EventArgs e)
+        {            
+            gkh.HookedKeys.Add(Keys.Left);
+            gkh.HookedKeys.Add(Keys.Right);
+            gkh.HookedKeys.Add(Keys.Up);
+            gkh.HookedKeys.Add(Keys.Down);
+            gkh.HookedKeys.Add(Keys.PageUp);
+            gkh.HookedKeys.Add(Keys.PageDown);
+            gkh.HookedKeys.Add(Keys.Escape);
+            gkh.HookedKeys.Add(Keys.W);
+            gkh.HookedKeys.Add(Keys.S);
+            gkh.HookedKeys.Add(Keys.A);
+            gkh.HookedKeys.Add(Keys.D);
+            gkh.KeyDown += new KeyEventHandler(gkh_KeyDown);
+            gkh.KeyUp += new KeyEventHandler(gkh_KeyUp);    
+            
+            if (MainV2.keyboard)
+            {
+                BUT_enable.Text = "Disable";
+                timer1.Start();
+            }
+        }
+
+        private void BUT_enable_Click(object sender, EventArgs e)
+        {
+            if (MainV2.keyboard == false)
+            {
+                MainV2.comPort.MAV.cs.rcoverridech1 = checkChannel(1, "trim");
+                MainV2.comPort.MAV.cs.rcoverridech2 = checkChannel(2, "trim");
+                MainV2.comPort.MAV.cs.rcoverridech3 = checkChannel(3, "trim");
+                MainV2.comPort.MAV.cs.rcoverridech4 = checkChannel(4, "trim");
+                gkh.hook();
+                MainV2.keyboard = true;
+                BUT_enable.Text = "Disable";
+            }
+            else
+            {
+                gkh.unhook();
+                MainV2.keyboard = false;
+                clearRCOverride();
+                BUT_enable.Text = "Enable";
+            }
+        }
+
+        void gkh_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    MainV2.comPort.MAV.cs.rcoverridech1 = (ushort)(1500);
+                    break;
+                case Keys.Right:
+                    MainV2.comPort.MAV.cs.rcoverridech1 = (ushort)(1500);
+                    break;
+                case Keys.Up:
+                    MainV2.comPort.MAV.cs.rcoverridech2 = (ushort)(1500);
+                    break;
+                case Keys.Down:
+                    MainV2.comPort.MAV.cs.rcoverridech2 = (ushort)(1500);
+                    break;
+                case Keys.W:
+                    MainV2.comPort.MAV.cs.rcoverridech3 = (ushort)(1425);
+                    break;
+                case Keys.S:
+                    MainV2.comPort.MAV.cs.rcoverridech3 = (ushort)(1425);
+                    break;
+                case Keys.A:
+                    MainV2.comPort.MAV.cs.rcoverridech4 = (ushort)(1500);
+                    break;
+                case Keys.D:
+                    MainV2.comPort.MAV.cs.rcoverridech4 = (ushort)(1500);
+                    break;
+            }
+       
+        }
+
+        void gkh_KeyDown(object sender, KeyEventArgs e) 
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.PageUp:
+                    MainV2.instance.BeginInvoke((System.Windows.Forms.MethodInvoker)delegate()
+                    {
+                        try
+                        {
+                            MainV2.comPort.doARM(true);
+                        }
+                        catch { CustomMessageBox.Show("Failed to Arm"); }
+                    });
+                    break;
+                case Keys.Left:
+                    /*if (MainV2.comPort.MAV.cs.rcoverridech1 > checkChannel(1, "min"))
+                        MainV2.comPort.MAV.cs.rcoverridech1 -= (ushort)(100);*/
+                    MainV2.comPort.MAV.cs.rcoverridech1 = (ushort)(1400);
+                    break;
+                case Keys.Right:
+                    /*if (MainV2.comPort.MAV.cs.rcoverridech1 < checkChannel(1, "max"))
+                        MainV2.comPort.MAV.cs.rcoverridech1 += (ushort)(100);*/
+                    MainV2.comPort.MAV.cs.rcoverridech1 = (ushort)(1600);
+                    break;
+                case Keys.Up:
+                    /*if (MainV2.comPort.MAV.cs.rcoverridech2 > checkChannel(2, "min"))
+                        MainV2.comPort.MAV.cs.rcoverridech2 -= (ushort)(100);*/
+                    MainV2.comPort.MAV.cs.rcoverridech2 = (ushort)(1400);
+                    break;
+                case Keys.Down:
+                    /*if (MainV2.comPort.MAV.cs.rcoverridech2 < checkChannel(2, "max"))
+                        MainV2.comPort.MAV.cs.rcoverridech2 += (ushort)(100);*/
+                    MainV2.comPort.MAV.cs.rcoverridech2 = (ushort)(1600);
+                    break;
+                case Keys.W:
+                    if (MainV2.comPort.MAV.cs.rcoverridech3 < checkChannel(3, "max"))
+                        MainV2.comPort.MAV.cs.rcoverridech3 += (ushort)(100);
+                    break;
+                case Keys.S:
+                    if (MainV2.comPort.MAV.cs.rcoverridech3 > checkChannel(3, "min"))
+                        MainV2.comPort.MAV.cs.rcoverridech3 -= (ushort)(100);
+                    break;
+                case Keys.A:
+                    /*if (MainV2.comPort.MAV.cs.rcoverridech4 > checkChannel(4, "min"))
+                        MainV2.comPort.MAV.cs.rcoverridech4 -= (ushort)(100);*/
+                    MainV2.comPort.MAV.cs.rcoverridech4 = (ushort)(1400);
+                    break;
+                case Keys.D:
+                    /*if (MainV2.comPort.MAV.cs.rcoverridech4 < checkChannel(4, "max"))
+                        MainV2.comPort.MAV.cs.rcoverridech4 += (ushort)(100);*/
+                    MainV2.comPort.MAV.cs.rcoverridech4 = (ushort)(1600);
+                    break;
+                case Keys.Escape:
+                    MainV2.comPort.MAV.cs.rcoverridech1 = (ushort)0;
+                    MainV2.comPort.MAV.cs.rcoverridech2 = (ushort)0;
+                    MainV2.comPort.MAV.cs.rcoverridech3 = (ushort)0;
+                    MainV2.comPort.MAV.cs.rcoverridech4 = (ushort)0;
+                    clearRCOverride();
+                    break;
+            }            
+        }
+
+        public void clearRCOverride()
+        {
+
+            MAVLink.mavlink_rc_channels_override_t rc = new MAVLink.mavlink_rc_channels_override_t();
+
+            rc.target_component = MainV2.comPort.MAV.compid;
+            rc.target_system = MainV2.comPort.MAV.sysid;
+
+            rc.chan1_raw = 0;
+            rc.chan2_raw = 0;
+            rc.chan3_raw = 0;
+            rc.chan4_raw = 0;
+            rc.chan5_raw = 0;
+            rc.chan6_raw = 0;
+            rc.chan7_raw = 0;
+            rc.chan8_raw = 0;
+
+            try
+            {
+                MainV2.comPort.sendPacket(rc);
+                MainV2.comPort.sendPacket(rc);
+                MainV2.comPort.sendPacket(rc);
+            }
+            catch { }
+        }
+
+        private ushort checkChannel(int chan, string minmaxtrim)
+        {
+            ushort min, max, trim = 0;
+            if (MainV2.comPort.MAV.param.Count > 0)
+            {
+                try
+                {
+                    if (MainV2.comPort.MAV.param.ContainsKey("RC" + chan + "_MIN"))
+                    {
+                        min = (ushort)(float)(MainV2.comPort.MAV.param["RC" + chan + "_MIN"]);
+                        max = (ushort)(float)(MainV2.comPort.MAV.param["RC" + chan + "_MAX"]);
+                        trim = (ushort)(float)(MainV2.comPort.MAV.param["RC" + chan + "_TRIM"]);
+                    }
+                    else
+                    {
+                        min = 1000;
+                        max = 2000;
+                        trim = 1500;
+                    }
+                }
+                catch
+                {
+                    min = 1000;
+                    max = 2000;
+                    trim = 1500;
+                }
+            }
+            else
+            {
+                min = 1000;
+                max = 2000;
+                trim = 1500;
+            }
+            if (minmaxtrim == "min")
+                return min;
+            if (minmaxtrim == "max")
+                return max;
+            if (minmaxtrim == "trim")
+                return trim;
+            else
+                return 0;
+        }
+
+        /*protected override bool IsInputKey(Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.Right:
+                case Keys.Left:
+                case Keys.Up:
+                case Keys.Down:
+                    return true;
+                case Keys.Shift | Keys.Right:
+                case Keys.Shift | Keys.Left:
+                case Keys.Shift | Keys.Up:
+                case Keys.Shift | Keys.Down:
+                    return true;
+            }
+            return base.IsInputKey(keyData);
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+            MAVLink.mavlink_rc_channels_override_t rc = new MAVLink.mavlink_rc_channels_override_t();
+            rc.target_component = MainV2.comPort.MAV.compid;
+            rc.target_system = MainV2.comPort.MAV.sysid;
+            rc.chan1_raw = 0;
+            rc.chan2_raw = 0;
+            rc.chan3_raw = 0;
+            rc.chan4_raw = 0;
+            rc.chan5_raw = 0;
+            rc.chan6_raw = 0;
+            rc.chan7_raw = 0;
+            rc.chan8_raw = 0;
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    //MainV2.comPort.MAV.cs.rcoverridech1 = (ushort)(1400);
+                    rc.chan1_raw = (ushort)(1400);
+                    MainV2.comPort.sendPacket(rc);
+                    break;
+                case Keys.Right:
+                    //MainV2.comPort.MAV.cs.rcoverridech1 = (ushort)(1600);
+                    rc.chan1_raw = (ushort)(1600);
+                    MainV2.comPort.sendPacket(rc);
+                    break;
+                case Keys.Up:
+                    //MainV2.comPort.MAV.cs.rcoverridech2 = (ushort)(1400);
+                    rc.chan2_raw = (ushort)(1400);
+                    MainV2.comPort.sendPacket(rc);
+                    break;
+                case Keys.Down:
+                    //MainV2.comPort.MAV.cs.rcoverridech2 = (ushort)(1600);
+                    rc.chan2_raw = (ushort)(1600);
+                    MainV2.comPort.sendPacket(rc);
+                    break;
+                case Keys.Escape:
+                    //MainV2.comPort.MAV.cs.rcoverridech1 = (ushort)0;
+                    //MainV2.comPort.MAV.cs.rcoverridech2 = (ushort)0;
+                    //MainV2.joystick.clearRCOverride();
+                    rc.chan1_raw = (ushort)(0);
+                    rc.chan2_raw = (ushort)(0);
+                    MainV2.comPort.sendPacket(rc);
+                    break;
+            }
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            base.OnKeyUp(e);
+            MAVLink.mavlink_rc_channels_override_t rc = new MAVLink.mavlink_rc_channels_override_t();
+            rc.target_component = MainV2.comPort.MAV.compid;
+            rc.target_system = MainV2.comPort.MAV.sysid;
+            rc.chan1_raw = 0;
+            rc.chan2_raw = 0;
+            rc.chan3_raw = 0;
+            rc.chan4_raw = 0;
+            rc.chan5_raw = 0;
+            rc.chan6_raw = 0;
+            rc.chan7_raw = 0;
+            rc.chan8_raw = 0;
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    //MainV2.comPort.MAV.cs.rcoverridech1 = (ushort)(1500);
+                    rc.chan1_raw = (ushort)(1500);
+                    MainV2.comPort.sendPacket(rc);
+                    break;
+                case Keys.Right:
+                    //MainV2.comPort.MAV.cs.rcoverridech1 = (ushort)(1500);
+                    rc.chan1_raw = (ushort)(1500);
+                    MainV2.comPort.sendPacket(rc);
+                    break;
+                case Keys.Up:
+                    //MainV2.comPort.MAV.cs.rcoverridech2 = (ushort)(1500);
+                    rc.chan2_raw = (ushort)(1500);
+                    MainV2.comPort.sendPacket(rc);
+                    break;
+                case Keys.Down:
+                    //MainV2.comPort.MAV.cs.rcoverridech2 = (ushort)(1500);
+                    rc.chan2_raw = (ushort)(1500);
+                    MainV2.comPort.sendPacket(rc);
+                    break;
+            }
+        }*/
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBarRoll.Value = MainV2.comPort.MAV.cs.rcoverridech1;
+            progressBarPitch.Value = MainV2.comPort.MAV.cs.rcoverridech2;
+            progressBarThrottle.Value = MainV2.comPort.MAV.cs.rcoverridech3;
+            progressBarYaw.Value = MainV2.comPort.MAV.cs.rcoverridech4;
+
+            try
+            {
+                progressBarRoll.Maximum = checkChannel(1,"max");
+                progressBarPitch.Maximum = checkChannel(2, "max");
+                progressBarThrottle.Maximum = checkChannel(3, "max");
+                progressBarYaw.Maximum = checkChannel(4, "max");
+
+                progressBarRoll.Minimum = checkChannel(1, "min");
+                progressBarPitch.Minimum = checkChannel(2, "min");
+                progressBarThrottle.Minimum = checkChannel(3, "min");
+                progressBarYaw.Minimum = checkChannel(4, "min");
+            
+            }
+            catch
+            {
+                //Exception Error in the application. -2147024866 (DIERR_INPUTLOST)
+
+            }
+        }
+    }
+}
